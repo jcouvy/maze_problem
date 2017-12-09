fcf383fe8dcb6b3cd4580cca0db7c769e48f9e41
jcouvy <julien.couvy@gmail.com>
2017-12-09 21:42:58 +0700

    Graph class: findNeighbours (WIP)

---
+++ Graph.java
@@ -0,0 +1,66 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Graph {
+
+   private Map<Integer, Vertex> vertices;
+   private char[][] maze;
+
+   public Graph(Map<Integer, Vertex> m, char[][] maze)
+   {
+       vertices = m.clone();
+       maze = maze.clone();
+   }
+
+   public boolean isVertex(Position p)
+   {
+       //...
+   }
+
+   public boolean existsEdgeBetween(Vertex v1, Vertex v2)
+   {
+       //...
+   }
+
+   public int getIdFromPosition(Position p)
+   {
+       return p.getX() * maze[0].length + p.getY();
+   }
+
+   public int calcWeight(Vertex v1, Vertex v2)
+   {
+       Position p1 = v1.getPos();
+       Position p2 = v2.getPos();
+   }
+
+   public void findNeighbours(Position initialPos)
+   {
+       if (!existsPathFrom(initialPos)
+           return;
+       else {
+           if (isVertex(initialPos)) {
+               int id = getIdFromPosition(initialPos);
+               Vertex v = new Vertex(initialPos, id);
+               if (!existsEdgeBetween()) {
+                   int weight = calcWeight()
+                   Edge e = new Edge(, , weight);
+               }
+           }
+       }
+   }
+
+    /* Checks wether it is possible to move from a given Vertex v
+    * This means there is a clear way either UP, DOWN, LEFT, or RIGHT.
+    */
+   private boolean existsPathFrom(Position p)
+   {
+       int posX = p.getX();
+       int posY = p.getY();
+       boolean result = false;
+       try {
+           result = maze[posX][posY] == '.' || maze[posX][posY] == '.' ||
+                    maze[posX][posY] == '.' || maze[posX][posY] == '.');
+       } catch IndexOutOfBoundsException e ();
+       return result;
+   }
+}
