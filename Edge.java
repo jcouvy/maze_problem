3c5b608e5d3bd5a18abdf2b3775750197a0feee8
jcouvy <julien.couvy@gmail.com>
2017-12-09 21:41:34 +0700

    Edge class implementation

---
+++ Edge.java
@@ -0,0 +1,29 @@
+public class Edge {
+
+    private Vertex start;
+    private Vertex end;
+    private final int weight;
+
+    public Edge(Vertex s, Vertex e, int w)
+    {
+        start = s;
+        end = e;
+        weight = w;
+    }
+
+    public Vertex getStart()
+    {
+        return start;
+    }
+
+    public Vertex getEnd()
+    {
+        return end;
+    }
+
+    public String toString()
+    {
+        return "(" + end.getId() + "," + weight + ")"
+    }
+
+}
