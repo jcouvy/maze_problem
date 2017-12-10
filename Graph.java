326b2a7a78e20edfbf4494ecb01fd4bbd33cb3b4
jcouvy <julien.couvy@gmail.com>
2017-12-10 16:50:25 +0700

    Methods to find if a given position is a Vertex

--- Graph.java
+++ Graph.java
@@ -1,26 +1,111 @@
 import java.util.HashMap;
 
-
 public class Graph {
 
-    public HashMap<Integer, Vertex> getVertices() {
-        return vertices;
-    }
-
-    private HashMap<Integer, Vertex> vertices;
+   private HashMap<Integer, Vertex> vertices;
    private char[][] maze;
+   private final char WALL = '#';
+   private final char PATH = '.';
 
    public Graph(HashMap<Integer, Vertex> m, char[][] maze)
    {
-       //vertices = m.clone();
-       this.vertices = (HashMap)m.clone();
+       this.vertices = (HashMap) m.clone();
        this.maze = maze.clone();
    }
 
+   public HashMap<Integer, Vertex> getVertices() {
+       return vertices;
+   }
+
+   /* In the maze problem, a Vertex is a station located
+      - At the intersection between two routes
+      - At the end of a route (entry or exit)
+      Returns whether the Position p on the maze corresponds to
+      a Vertex.
+    */
    public boolean isVertex(Position p)
    {
-       //...
-       return false;
+       return isAtEnd(p) || isAtIntersection(p);
+   }
+
+   /* Checks if a given Position p is an accessible cell
+   in the maze (i.e: is a '.')
+    */
+   public boolean isAccessible(Position p)
+   {
+       boolean accessible = false;
+
+       try {
+          accessible = maze[p.getX()][p.getY()] == PATH;
+       } catch (IndexOutOfBoundsException outOfBonds) {};
+
+       return accessible;
+   }
+
+    /* Checks if a given Position p is at an intersection of routes.
+       An intersection is found if there exists both an horizontal and vertical
+       path around the position.
+     */
+   public boolean isAtIntersection(Position p)
+   {
+       boolean verticalPath = false;
+       boolean horizontalPath = false;
+       int posX = p.getX();
+       int posY = p.getY();
+
+       try {
+           if (isAccessible(new Position(posX-1, posY)) ||
+               isAccessible(new Position(posX+1, posY)) verticalPath = true;
+           if (isAccessible(new Position(posX, posY-1)) ||
+               isAccessible(new Position(posX, posY+1)) horizonPath = true;
+       } catch (IndexOutOfBoundsException outOfBonds) {};
+
+       return verticalPath && horizontalPath;
+   }
+
+   /* Checks if a Position p is at the beginning/end of a route.
+      A beginning/end is found either:
+       - if the position is accessible and located at the border of the maze
+       - if there are three walls and one accessible way (where we come from)
+         surrounding the position.
+    */
+   public boolean isAtEnd(Position p)
+   {
+       int surroundingWalls = 0;
+       int accessiblePaths = 0;
+
+       if isAtBorder(p) return true;
+       else {
+           try {
+               if (isAccessible(new Position(posX-1, posY)) accessiblePaths++;
+               else surroundingWalls++;
+               if (isAccessible(new Position(posX+1, posY)) accessiblePaths++;
+               else surroundingWalls++;
+               if (isAccessible(new Position(posX, posY-1)) accessiblePaths++;
+               else surroundingWalls++;
+               if (isAccessible(new Position(posX, posY+1)) accessiblePaths++;
+               else surroundingWalls++;
+           } catch (IndexOutOfBoundsException outOfBonds) {};
+       }
+
+       return surroundingWalls == 3 && accessiblePaths == 1;
+   }
+
+   /* Checks if a Position is accessible and located at the border of the maze */
+   public boolean isAtBorder(Position p)
+   {
+       boolean border = false;
+       int posX = p.getX();
+       int posY = p.getY();
+
+       if (posX == 0 || posX == maze.length) {
+           if (isAccessible(p)) border = true;
+       }
+       if (posY == 0 || posY == maze[0].length) {
+           if (isAccessible(p)) border = true;
+       }
+
+       return border;
    }
 
    public boolean existsEdgeBetween(Vertex v1, Vertex v2)
