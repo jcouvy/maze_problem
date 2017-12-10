10c4e739c8410365b16b01f0e3a5c5b212f4bc12
Antoine Barriere <antoine.barriere@etu.u-bordeaux.fr>
2017-12-10 18:34:15 +0700

    .gitignore modification

--- Main.java
+++ Main.java
@@ -45,17 +45,18 @@ public class Main {
         System.out.println(vertexMap);
 
         // Check is vertex
+
         Position postest = new Position(0,0);
-        System.out.println(g.isVertex(postest));
-        for(int i = 0; i<row;i++){
+        System.out.println(g.isAtEnd(postest));
+        /*for(int i = 0; i<row;i++){
             for(int j=0;j<col;j++){
                 postest = new Position(i,j);
                 if(g.isVertex(postest)){
                     System.out.println(postest.toString());
                 }
             }
-        }
-        //
+        }*/
+
 
     }
 }
